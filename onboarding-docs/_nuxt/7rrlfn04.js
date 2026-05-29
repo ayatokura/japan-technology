edac744b5c5423f4c06d134465052d589b0ab958
Mizuho Uehara <mu@muAir.local>
2026-05-29 15:59:12 +0900

    Update onboarding-docs

--- onboarding-docs/_nuxt/7rrlfn04.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./8K29yBHY.js";import{r as e,c as i}from"./xcOzXci7.js";const[n]=o("ConfigProvider");function s(r){const t=n({dir:e("ltr")});return i(()=>r?.value||t.dir?.value||"ltr")}export{n as i,s as u};
---
+++ onboarding-docs/_nuxt/BkuCM0Xv.js
@@ -0,0 +1,1 @@
+import{c as o}from"./DZa2o7ZQ.js";import{r as e,c as i}from"./CSnekf2L.js";const[n]=o("ConfigProvider");function s(r){const t=n({dir:e("ltr")});return i(()=>r?.value||t.dir?.value||"ltr")}export{n as i,s as u};
